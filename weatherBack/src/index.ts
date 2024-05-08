bc0ac93f165fe1140456068a46f70dec1c0a7404
daniel <danielsss3131@gmail.com>
2024-05-08 17:18:01 +0300

    make the backend work - send the whole json object with the weather, also create front project with react typescript and vite

--- weatherBack/src/index.ts
+++ weatherBack/src/index.ts
@@ -1,26 +1,30 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-
+const cors = require("cors");
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
-const API_KEY = process.env.API_KEY
+const API_KEY = process.env.API_KEY;
+app.use(cors);
 
 //https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=b9f792786ad37f486cf4a129f6e0d8c6
 
 //TODO: send 2 variables to the function, and use them in the api call (lat and lon)
-const getWeather  = async (lat: number, lon: number) => {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+const getWeather = async (lat: number, lon: number) => {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+  );
+  const weatherData = await response.json().catch((e) => "error");
 
-  const data = await response.json()
-  console.log(data)
-   return data;
-}
+  return weatherData;
+};
 
 //todo: make sure the client sees the json like we see it in the terminal
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + waadwadd sdsd\n" + getWeather(44.34, 10.9));
+app.get("/", async (req: Request, res: Response) => {
+  const data = await getWeather(44.34, 10.9);
+
+  res.send(data);
 });
 
 app.listen(port, () => {
