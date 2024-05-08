277ec44792798118a689862374514673114b32d7
daniel <danielsss3131@gmail.com>
2024-05-08 13:42:02 +0300

    use variables

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,18 @@ const API_KEY = process.env.API_KEY
 
 //https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=b9f792786ad37f486cf4a129f6e0d8c6
 
-const getWeather  = async () => {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${API_KEY}`)
+//TODO: send 2 variables to the function, and use them in the api call (lat and lon)
+const getWeather  = async (lat: number, lon: number) => {
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
 
   const data = await response.json()
   console.log(data)
    return data;
 }
 
+//todo: make sure the client sees the json like we see it in the terminal
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + waadwadd sdsd\n" + getWeather());
+  res.send("Express + waadwadd sdsd\n" + getWeather(44.34, 10.9));
 });
 
 app.listen(port, () => {
